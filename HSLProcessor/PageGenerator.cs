d7f4094b6a51a400629453dfe6cdffb130ff1ec7
Hideki Saito <hideki@hidekisaito.com>
2017-05-28 04:36:56 -0700

    Support for date stamps

--- HSLProcessor/PageGenerator.cs
+++ HSLProcessor/PageGenerator.cs
@@ -85,6 +85,8 @@ namespace HSLProcessor
                     item_template = item_template.Replace("{{title}}", item.Title);
                     item_template = item_template.Replace("{{content}}", title_detail);
 
+                    item_template = item_template.Replace("{{generated}}", DateTime.UtcNow.ToString("u"));
+
                     var writer = new StreamWriter(new FileStream(directory.FullName + "/" + item.TitleId + ".html", FileMode.Create), Encoding.UTF8);
                     writer.Write(item_template);
                     writer.Flush();
@@ -149,6 +151,8 @@ namespace HSLProcessor
                     song_list_content += "</table>";
 
                     item_template = item_template.Replace("{{content}}",song_list_content);
+
+                    item_template = item_template.Replace("{{generated}}", DateTime.UtcNow.ToString("u"));
                     
                     var writer = new StreamWriter(new FileStream(directory.FullName + "/" + item.ArtistId + ".html", FileMode.Create), Encoding.UTF8);
                     writer.Write(item_template);
@@ -215,6 +219,8 @@ namespace HSLProcessor
 
                     item_template = item_template.Replace("{{content}}",song_list_content);
                     
+                    item_template = item_template.Replace("{{generated}}", DateTime.UtcNow.ToString("u"));
+
                     var writer = new StreamWriter(new FileStream(directory.FullName + "/" + item.SourceId + ".html", FileMode.Create), Encoding.UTF8);
                     writer.Write(item_template);
                     writer.Flush();
@@ -262,7 +268,10 @@ namespace HSLProcessor
                 var songs_template_reader = new StreamReader(new FileStream(current_dir + @"/templates/songs.tpl", FileMode.Open));
                 var songs_template = songs_template_reader.ReadToEnd();
 
-                songs_template = songs_template.Replace("{{count}}", song_list.Count.ToString());
+                songs_template = songs_template.Replace("{{title_count}}", context.Songs.Count().ToString());
+                songs_template = songs_template.Replace("{{artist_count}}", context.Artists.Count().ToString());
+                songs_template = songs_template.Replace("{{source_count}}", context.Sources.Count().ToString());
+
                 var song_list_output = "<table id=\"content_table\"><tr class=\"row\"><th class=\"cell table_head\">Title</th><th class=\"cell table_head\">Artist</th><th class=\"cell table_head\">Source</th></tr>\r\n{{content}}</table>";
                 var song_list_content = "";
 
@@ -275,6 +284,8 @@ namespace HSLProcessor
                 song_list_output = song_list_output.Replace("{{content}}", song_list_content);
 
                 songs_template = songs_template.Replace("{{content}}", song_list_output);
+                
+                songs_template = songs_template.Replace("{{generated}}", DateTime.UtcNow.ToString("u"));
 
                 var writer = new StreamWriter(new FileStream(directory.FullName + "/songs.html", FileMode.Create), Encoding.UTF8);
                 writer.Write(songs_template);
