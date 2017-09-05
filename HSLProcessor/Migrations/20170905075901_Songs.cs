1cc881d0ae1dab261b2da4fccf842146fd3f6b56
Hideki Saito <hideki@hidekisaito.com>
2017-09-05 00:59:39 -0700

    Updated DB migrations

---
+++ HSLProcessor/Migrations/20170905075901_Songs.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HSLProcessor.Migrations
+{
+    public partial class Songs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Artists",
+                columns: table => new
+                {
+                    ArtistId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Artists", x => x.ArtistId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Series",
+                columns: table => new
+                {
+                    SeriesId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Series", x => x.SeriesId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Sources",
+                columns: table => new
+                {
+                    SourceId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    SeriesId = table.Column<Guid>(type: "BLOB", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Sources", x => x.SourceId);
+                    table.ForeignKey(
+                        name: "FK_Sources_Series_SeriesId",
+                        column: x => x.SeriesId,
+                        principalTable: "Series",
+                        principalColumn: "SeriesId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Songs",
+                columns: table => new
+                {
+                    TitleId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    ArtistId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    SourceId = table.Column<Guid>(type: "BLOB", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Songs", x => x.TitleId);
+                    table.ForeignKey(
+                        name: "FK_Songs_Artists_ArtistId",
+                        column: x => x.ArtistId,
+                        principalTable: "Artists",
+                        principalColumn: "ArtistId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Songs_Sources_SourceId",
+                        column: x => x.SourceId,
+                        principalTable: "Sources",
+                        principalColumn: "SourceId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_ArtistId",
+                table: "Songs",
+                column: "ArtistId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Songs_SourceId",
+                table: "Songs",
+                column: "SourceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Sources_SeriesId",
+                table: "Sources",
+                column: "SeriesId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Songs");
+
+            migrationBuilder.DropTable(
+                name: "Artists");
+
+            migrationBuilder.DropTable(
+                name: "Sources");
+
+            migrationBuilder.DropTable(
+                name: "Series");
+        }
+    }
+}
